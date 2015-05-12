7ee59ff7ef3ddf66a6d1a447c3a98ed90417efd0
alainncls <alain.nicolas@epfedu.fr>
2015-05-12 14:19:41 +0200

    Add book to cart

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,23 @@
 'use strict';
 
 angular.module('bibliothequeApp')
-  .controller('MainCtrl', function ($scope, $http, $rootScope) {
+  .controller('MainCtrl', function ($scope, $http, cartService) {
     $scope.books = [];
 
     $http.get('http://henri-potier.xebia.fr/books').success( function (data) {
       $scope.books = data;
     });
 
-    if($rootScope.cart == []){
-
+    function findBookByIsbn(isbn){
+      angular.forEach(books, function(index, value){
+        if (value.isbn == isbn){
+          return value;
+        }
+      })
     }
 
+    $scope.addToCart = function (book){
+      console.log(book);
+      cartService.addBook(book)
+    }
   });
