a1668e01103d52c373ee2f0aa41496c67e34f68b
alainncls <alain.nicolas@epfedu.fr>
2015-05-21 16:50:02 +0200

    [TEST] MainController tests

--- test/spec/controllers/main.spec.js
+++ test/spec/controllers/main.spec.js
@@ -1,20 +1,60 @@
-describe('MainCtrl', function() {
-  var $scope, controller;
+describe('MainCtrl', function () {
+  var $scope, $location, controller, cartService;
 
   beforeEach(module('bibliothequeApp'));
+
   beforeEach(inject(function ($rootScope, $controller) {
+    $location = jasmine.createSpyObj('$location', ['path']);
     $scope = $rootScope.$new();
     controller = $controller('MainCtrl', {
-      $scope: $scope
+      $scope: $scope,
+      $location: $location
     });
   }));
 
-  it('should set books var', function() {
+  beforeEach(inject(function (_cartService_) {
+    cartService = _cartService_;
+    cartService.init();
+  }));
+
+  it('should contain a cartService', function () {
+    expect(cartService).toBeDefined();
+  });
+
+  it('should set books var', function () {
     expect($scope.books).toBeDefined();
   });
 
-  it('should call http://henri-potier.xebia.fr/books on $http', inject(function($httpBackend) {
+  it('should set addToCart() function', function () {
+    expect($scope.addToCart).toBeDefined();
+  });
+
+  it('should set goTo()', function () {
+    expect($scope.goTo).toBeDefined();
+  });
+
+  it('should get books from http://henri-potier.xebia.fr/books on $http', inject(function ($httpBackend) {
     $httpBackend.expectGET('http://henri-potier.xebia.fr/books').respond();
     $httpBackend.flush();
   }));
+
+  describe('$scope.addToCart', function () {
+    it('should add a book to the cart', function () {
+      $scope.addToCart({
+        'isbn': "c8fabf68-8374-48fe-a7ea-a00ccd07afff",
+        'title': "Henri Potier à l'école des sorciers",
+        'price': 35,
+        'cover': "http://henri-potier.xebia.fr/hp0.jpg"
+      });
+      expect(cartService.getCartSize()).toEqual(1);
+    });
+  });
+
+  describe('$scope.goTo', function () {
+    it('should go to the book page', inject(function ($httpBackend) {
+      var isbn = 'c8fabf68-8374-48fe-a7ea-a00ccd07afff';
+      $scope.goTo(isbn);
+      expect($location.path).toHaveBeenCalledWith('/book/' + isbn);
+    }));
+  });
 });
