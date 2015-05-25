7f3cbc95b1c706cd1121ae2fd32ac8f61b6a2ee8
alainncls <alain.nicolas.perso@gmail.com>
2015-05-25 18:31:26 +0200

    [TEST] CartController tests (passing)

--- test/spec/controllers/cart.spec.js
+++ test/spec/controllers/cart.spec.js
@@ -1,5 +1,28 @@
 describe('CartCtrl', function () {
   var $scope, $location, controller, cartService;
+  var book1 = {
+    'isbn': "c8fabf68-8374-48fe-a7ea-a00ccd07afff",
+    'title': "Henri Potier à l'école des sorciers",
+    'price': 35,
+    'cover': "http://henri-potier.xebia.fr/hp0.jpg"
+  };
+  var book2 = {
+    "isbn": "a460afed-e5e7-4e39-a39d-c885c05db861",
+    "title": "Henri Potier et la Chambre des secrets",
+    "price": 30,
+    "cover": "http://henri-potier.xebia.fr/hp1.jpg"
+  };
+  var offer1 = {
+    "type": "percentage",
+    "value": 4,
+    "total": 62.40
+  };
+
+  var offer2 = {
+    "type": "minus",
+    "value": 15,
+    "total": 50
+  };
 
   beforeEach(module('bibliothequeApp'));
 
@@ -10,25 +33,15 @@ describe('CartCtrl', function () {
       $scope: $scope,
       $location: $location
     });
+    $scope.offers = [offer1, offer2];
   }));
 
   beforeEach(inject(function (_cartService_) {
     cartService = _cartService_;
     cartService.init();
-    cartService.addBook({
-      'isbn': "c8fabf68-8374-48fe-a7ea-a00ccd07afff",
-      'title': "Henri Potier à l'école des sorciers",
-      'price': 35,
-      'cover': "http://henri-potier.xebia.fr/hp0.jpg",
-      'quantity': 2
-    });
-    cartService.addBook({
-      "isbn": "a460afed-e5e7-4e39-a39d-c885c05db861",
-      "title": "Henri Potier et la Chambre des secrets",
-      "price": 30,
-      "cover": "http://henri-potier.xebia.fr/hp1.jpg",
-      'quantity': 1
-    });
+    cartService.addBook(book1);
+    cartService.addBook(book2);
+    $scope.cart = cartService.getCart();
   }));
 
   it('should contain a cartService', function () {
@@ -75,54 +88,64 @@ describe('CartCtrl', function () {
     expect($scope.chooseBestOffer).toBeDefined();
   });
 
-  // Unable to perform this test at the moment...
+  // //Unable to perform this test at the moment...
   //it('should get offers from http://henri-potier.xebia.fr/books/{isbn}/commercialOffers on $http', inject(function ($httpBackend) {
-  //  cartService.addBook({
-  //    'isbn': "c8fabf68-8374-48fe-a7ea-a00ccd07afff",
-  //    'title': "Henri Potier à l'école des sorciers",
-  //    'price': 35,
-  //    'cover': "http://henri-potier.xebia.fr/hp0.jpg"
-  //  });
-  //  cartService.addBook({
-  //    "isbn": "a460afed-e5e7-4e39-a39d-c885c05db861",
-  //    "title": "Henri Potier et la Chambre des secrets",
-  //    "price": 30,
-  //    "cover": "http://henri-potier.xebia.fr/hp1.jpg"
-  //  });
-  //
-  //  $scope.cart = cartService.getCart();
-  //
-  //  var isbn = $scope.isbnList();
-  //
-  //  $httpBackend.expectGET("http://henri-potier.xebia.fr/books/" + isbn + "/commercialOffers").respond();
+  //  $httpBackend.expectGET("http://henri-potier.xebia.fr/books/" + $scope.isbnList() + "/commercialOffers").respond();
   //  $httpBackend.flush();
   //}));
 
-  //describe('getTotalCart', function () {
-  //  it('should calculate the total amount of the cart', function () {
-  //    expect($scope.getTotalCart()).toEqual(100);
-  //  });
-  //});
+  describe('getTotalCart', function () {
+    it('should calculate the total amount of the cart', function () {
+      expect($scope.getTotalCart()).toEqual(65);
+    });
+  });
 
-  //describe('getTotalOffer', function () {
-  //  it('should calculate the total amount of the cart for a specific offer', function () {
-  //    var offer = {
-  //      "type": "minus",
-  //      "value": 15
-  //    };
-  //    expect($scope.getTotalOffer(offer.total)).toEqual(85);
-  //  });
-  //});
+  describe('getTotalOffer', function () {
+    it('should calculate the total amount of the cart for a specific offer', function () {
+      var offer = {
+        "type": "minus",
+        "value": 15
+      };
+      $scope.getTotalOffer(offer);
+      expect(offer.total).toEqual("50.00");
+    });
+  });
 
-  //describe('calculateOffers', function () {
-  //  it('should calculate the total amount of the cart for a specific offer', function () {
-  //    var offer = {
-  //      "type": "minus",
-  //      "value": 15
-  //    };
-  //    expect($scope.calculateOffers()).toEqual(true);
-  //  });
-  //});
+  describe('$scope.isbnList', function () {
+    it('should give the isbn list', function () {
+      expect($scope.isbnList()).toEqual('c8fabf68-8374-48fe-a7ea-a00ccd07afff,a460afed-e5e7-4e39-a39d-c885c05db861');
+    });
+  });
+
+  describe('$scope.deleteFromCart', function () {
+    it('should remove a book from the cart', function () {
+      $scope.deleteFromCart(book1);
+      expect($scope.cart.length).toEqual(1);
+    });
+  });
+
+  describe('$scope.incrQtity', function () {
+    it('should increment one book quantity', function () {
+      $scope.incrQtity(book2);
+      expect($scope.cart[1].quantity).toEqual(2);
+    });
+  });
+
+  describe('$scope.decrQtity', function () {
+    it('should decrement one book quantity', function () {
+      $scope.cart[1].quantity = 2;
+      $scope.decrQtity(book2);
+      expect($scope.cart[1].quantity).toEqual(1);
+    });
+  });
+
+  describe('$scope.chooseBestOffer', function () {
+    it('should say if one offer is the best', function () {
+      $scope.chooseBestOffer(offer1);
+      expect($scope.offers[0].best).toEqual(false);
+      expect($scope.offers[1].best).toEqual(true);
+    });
+  });
 
   describe('$scope.goTo', function () {
     it('should go to the book page', inject(function ($httpBackend) {
